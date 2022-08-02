b98c73bdd0cd032c3549d7ea534e986a2caaf028
jesusoyer <oyervidesjesus017@gmail.com>
2022-08-02 13:29:00 -0500

    added plugins

--- Develop/client/webpack.config.js
+++ Develop/client/webpack.config.js
@@ -14,15 +14,42 @@ module.exports = () => {
       install: './src/js/install.js'
     },
     output: {
-      filename: '[name].bundle.js',
+      filename: 'bundle.js',
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
+      new HtmlWebpackPlugin({
+        template: './index.html',
+        title: 'Webpack Plugin',
+      }),
+
+      // new InjectManifest({
+      //   swSrc: './src/src-sw.js',
+      //   swDest: 'service-worker.js',
+      // }),
       
     ],
 
     module: {
       rules: [
+        {
+          test:/\.css$/i,
+          use: ['style-loader','css-loader']
+        },
+        {
+          test:/\.(png|svg|jpg|jpeg|gif)$/i,
+          type: 'asset/resource',
+        },
+        {
+          test:/\.m?js$/,
+          exclude: /(node_modules|bower_components)/,
+          use: {
+            options:{
+              loader: 'babel-loader',
+              presets: ['@babel/preset-env']
+            }
+          }
+        }
         
       ],
     },
